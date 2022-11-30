5b724f8e460e3c8cc5002584add42533a6ea0c81
twesha.satia@searce.com <twesha.satia@searce.com>
2022-11-30 16:00:11 +0530

    Ninja Level 1

---
+++ Exercises-NinjaLevel1/5/hands_on_exercise_5.go
@@ -0,0 +1,45 @@
+// 1. at the package level scope, using the “var” keyword, create a VARIABLE with the
+// IDENTIFIER “y”. The variable should be of the UNDERLYING TYPE of your custom
+// TYPE “x”
+// a. eg:
+// 2. in func main
+// a. this should already be done
+// i. print out the value of the variable “x”
+// ii. print out the type of the variable “x”
+// iii. assign your own VALUE to the VARIABLE “x” using the “=” OPERATOR
+// iv. print out the value of the variable “x”
+
+// b. now do this
+// i. now use CONVERSION to convert the TYPE of the VALUE stored in “x”
+// to the UNDERLYING TYPE
+// 1. then use the “=” operator to ASSIGN that value to “y”
+// 2. print out the value stored in “y”
+// 3. print out the type of “y”
+
+package main
+
+import "fmt"
+
+type MyType int
+
+var x MyType
+
+// 1a
+var y int
+
+func main() {
+	//2a
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+
+	//2b1
+	y = int(x)
+
+	//2b2
+	fmt.Println(y)
+
+	//2b3
+	fmt.Printf("%T\n", x)
+}
